43a890e8def57712f944dcc090032cc2a747057d
Aayush <ikps562@gmail.com>
2026-09-10 11:48:50 +0530

    lab4 is started

---
+++ lab4/prg7.js
@@ -0,0 +1,22 @@
+import http from "http";
+const server = http.createServer((req, res) => {
+  res.setHeader("Content-Type", "application/json");
+
+  if (req.url === "/api/users" && req.method === "GET") {
+    res.end(JSON.stringify({ msg: "all Users" }));
+  } else if (req.url === "/api/users/1" && req.method === "POST") {
+    res.end(JSON.stringify({ msg: "add Users" }));
+  } else if (req.url === "/api/users/1" && req.method === "GET") {
+    res.end(JSON.stringify({ msg: "single user with id 1" }));
+  } else if (req.url === "/api/users/1" && req.method === "PUT") {
+    res.end(JSON.stringify({ msg: "update user 1 " }));
+  } else if (req.url === "/api/users/1" && req.method === "DELETE") {
+    res.end(JSON.stringify({ msg: "remove 1 " }));
+  } else {
+    res.statusCode = 404;
+    res.end(JSON.stringify({ msg: "Route not found" }));
+  }
+});
+server.listen(3000, () => {
+  console.log("prg 7 is Running...");
+});
